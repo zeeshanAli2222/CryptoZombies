897255c0d639c326af19f76af8862ac9d9128499
zeeshanAli2222 <109843086+zeeshanAli2222@users.noreply.github.com>
2022-08-28 06:24:53 +0500

    Update ERC20_Token.sol

--- cryptoWorking/ERC20_Token.sol
+++ cryptoWorking/ERC20_Token.sol
@@ -48,15 +48,7 @@ contract TokenERC20 {
         emit Transfer(_from, _to, _value);
         
     }
-
-    /**
-     * Transfer tokens
-     *
-     * Send `_value` tokens to `_to` from your account
-     *
-     * @param _to The address of the recipient
-     * @param _value the amount to send
-     */
+    //Transfer tokens
     function transfer(address _to, uint256 _value) public returns (bool success) {
         _transfer(msg.sender, _to, _value);
         return true;
